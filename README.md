4cefd110448065bb24a1d8c8b4ae3dcd2bbc93d6
George Weng <uncreativename0@gmail.com>
2022-12-25 02:19:44 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# dotfiles
+
+macOS bootstraping and syncing configs
+
+is this the best way to organize this? who knows, who cares.
+
+## Manual installs
+
+setup [yabai](https://github.com/koekeishiya/yabai/wiki) manually
+
---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/zsh
+
+echo "running bootstrap.sh"
+
+install() {
+    local name=${1}
+    local install_command=${2}
+
+    if ! type ${name} &> /dev/null; then
+        echo "Installing ${name}"
+        eval $install_command
+    else
+        echo "${name} already installed"
+    fi
+} 
+
+install "xcode-select" "xcode-select --install"
+install "brew" "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
+install "git" "brew install git"
+install "diff-so-fancy" "brew install diff-so-fancy"
+install "alacritty" "brew install alacritty"
+install "tmux" "brew install tmux"
+# install "zsh" "brew install zsh" # should already be installed by default
+
+if ! [ -d ~/.oh-my-zsh ]; then
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+ else
+ 	echo "oh-my-zsh already installed"
+fi
+
+install "code" "brew install --cask visual-studio-code"
+install "yabai" "brew install koekeishiya/formulae/yabai"
