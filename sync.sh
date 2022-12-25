08b917a28650bccf7ee661fe8c8ff26dc0501fe3
George Weng <uncreativename0@gmail.com>
2022-12-25 03:26:06 -0600

    sync.sh

---
+++ sync.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+
+echo "syncing dotfiles to ${0:a:h}"
+
+# -sf : create symbolic link, even if it already exists
+ln -sf ${0:a:h}/.alacritty.yml $HOME/.alacritty.yml
+ln -sf ${0:a:h}/.yabairc $HOME/.yabairc
+ln -sf ${0:a:h}/.zshrc $HOME/.zshrc
+ln -sf ${0:a:h}/.gitconfig $HOME/.gitconfig
