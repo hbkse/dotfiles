4dc5e615608034006f98b6817415344a894fcbe3
George Weng <uncreativename0@gmail.com>
2022-12-26 03:45:07 -0600

    fonts and colors

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,20 +14,30 @@ install() {
     fi
 } 
 
+# base
 install "xcode-select" "xcode-select --install"
 install "brew" "/bin/bash -c \"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)\""
 install "git" "brew install git"
-install "diff-so-fancy" "brew install diff-so-fancy"
+# install "zsh" "brew install zsh" # should already be installed by default
+install "code" "brew install --cask visual-studio-code"
+
+# terminal and shell enhancements
 install "alacritty" "brew install --cask alacritty"
 install "tmux" "brew install tmux"
-# install "zsh" "brew install zsh" # should already be installed by default
-
 if ! [ -d ~/.oh-my-zsh ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
  else
  	echo "oh-my-zsh already installed"
 fi
 
-install "code" "brew install --cask visual-studio-code"
+# window manager
 install "yabai" "brew install koekeishiya/formulae/yabai"
 install "skhd" "brew install koekeishiya/formulae/skhd"
+
+# colorful stuff
+install "lsd" "brew install lsd"
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+install "bat" "brew install bat"
+install "diff-so-fancy" "brew install diff-so-fancy"
